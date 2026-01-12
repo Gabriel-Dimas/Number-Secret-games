8105e67341f442e3a591787fca4bacd1e8d43467
Gabriel-Dimas <gabrieldimasm@gmail.com>
2026-01-12 16:34:46 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-let listaDeNumero;
 let numero = gerarNumeroAleatorio();
 let tentativas = 1;
 function exibirTextoNaTela(tag,Texto){
 
-   let compo = document.querySelector(tag);
-   compo.innerHTML = Texto; 
-   responsiveVoice.speak(Texto,'Brazilian Portuguese Female',{rate:1.2}); 
+let compo = document.querySelector(tag);
+compo.innerHTML = Texto; 
+responsiveVoice.speak(Texto,'Brazilian Portuguese Female',{rate:1.2}); 
 }
 
 function exibirTextoInicial(){ 
@@ -16,30 +15,31 @@ exibirTextoNaTela('p','Escolha um numero entre 1 a 10.');
 exibirTextoInicial();
 
 function verificarChute() {
-    let chut = document.querySelector('input').value;
+let chut = document.querySelector('input').value;
     
-    if (chut == numero ){
+if (chut == numero ){
         exibirTextoNaTela ('h1','Acertou!!');
         let palavraTentativa = tentativas > 1 ? 'tentativas': 'tentativa';
         let numeroDeTentativas = `Você descobreu o numero secreto com ${tentativas} ${palavraTentativa}!`;
         exibirTextoNaTela ('p', numeroDeTentativas);
         document.getElementById('reiniciar').removeAttribute('disabled');    
-    }else {
-        if (chut > numero){
-            exibirTextoNaTela ('p','O Numero e menor');
-        }else {exibirTextoNaTela ('p','O numero e maior ');
-
-        }
-        tentativas++;
-        liparCampo();
+}else {
+if (chut > numero){
+       exibirTextoNaTela ('p','O Numero e menor');
+}
+else {exibirTextoNaTela ('p','O numero e maior ');
+}
+        
+tentativas++;
+liparCampo();
     
-    }
+}
 
 }
 
 function gerarNumeroAleatorio() {
 
-  return parseInt(Math.random() * 10 + 1);
+return parseInt(Math.random() * 10 + 1);
         
 }
 
@@ -51,7 +51,8 @@ function reiniciarojogo() {
     numero = gerarNumeroAleatorio();
     liparCampo();
     tentativas = 1;
-   exibirTextoInicial();
-   document.getElementById('reiniciar').setAttribute('disabled',true);
+    exibirTextoInicial();
+    document.getElementById('reiniciar').setAttribute('disabled',true);
 } 
 console.log(numero);   
+
